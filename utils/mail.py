1e06840c466beaebfafb049096d6a1733cb77a8c
Angusdu <dydjw9@gmail.com>
2023-02-13 16:28:12 +0800

    update

--- utils/mail.py
+++ utils/mail.py
@@ -10,11 +10,11 @@ import socket
 
 SENDER = 'anonymousttnt@gmail.com'
 SMTP_SERVER = 'smtp.gmail.com'
-USER_ACCOUNT = {'username':'anonymousttnt@gmail.com', 'password':'11227788aa'}
+
 SUBJECT = "Test Test"
 
 
-def send_email(text,subject = socket.gethostname()+" reports",receivers=["anonymousttnt@gmail.com","dydjw9@gmail.com"], sender=SENDER, user_account=USER_ACCOUNT):
+def send_email(text,subject = socket.gethostname()+" reports",receivers=["anonymousttnt@gmail.com",""], sender=SENDER, user_account=USER_ACCOUNT):
     msg_root = MIMEMultipart()  # 创建一个带附件的实例
     msg_root['Subject'] = subject  # 邮件主题
     msg_root['To'] = COMMASPACE.join(receivers)  # 接收者
